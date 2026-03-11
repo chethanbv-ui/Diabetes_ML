858d75c9e2e165c518b7c2038b7f199bc32adc06
chethanbv-ui <chethanbv87@gmail.com>
2026-03-11 12:01:05 +0530

    updated diabetes

--- Diabetes_Predict.py
+++ Diabetes_Predict.py
@@ -15,7 +15,7 @@ y = df['diabetes']
 
 # Now split
 x_train, x_test, y_train, y_test = train_test_split(
-    x, y, test_size=0.2, random_state=42
+    x, y, test_size=0.25, random_state=42
 )
 
 # Train model
